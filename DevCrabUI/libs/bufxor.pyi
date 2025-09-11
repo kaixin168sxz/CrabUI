a878e1076ee1fbec378699a97a7def87816a8bd2
kaixin168sxz <122666772+kaixin168sxz@users.noreply.github.com>
2025-09-11 21:43:41 +0800

    将bufxor的out_buf与a_buf合并

--- DevCrabUI/libs/bufxor.pyi
+++ DevCrabUI/libs/bufxor.pyi
@@ -4,12 +4,11 @@
 last edited: 2025.8.27
 """
 
-def xor(out: bytearray, buf1: bytearray, buf2: bytearray):
+def xor(buf1: bytearray, buf2: bytearray):
     """
     对两个字节数组进行异或运算，结果存储在输出数组中
 
     Args:
-        out: 输出字节数组，存储异或结果
         buf1: 第一个输入字节数组
         buf2: 第二个输入字节数组
     """
